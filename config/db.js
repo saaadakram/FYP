700c613dda848234f53cdd9bfbb5fc9cc43a7e41
saadakram <saadakram288@gmail.com>
2025-11-30 21:30:11 +0500

    Final Year Project

--- config/db.js
+++ config/db.js
@@ -1,13 +1,13 @@
-const mongoose =require("mongoose")
-const colors=require("colors")
+const mongoose = require("mongoose");
+const colors = require("colors");
 
-const DataBaseConnection=async()=>{
-try {
-    await mongoose.connect(process.env.MONGO_URL)
-    console.log(`DataBase Connected`.bgBlue)
-} catch (error) {
-    console.log(`Server Faild ${error}`.bgRed)
-}
-
-}
-module.exports=DataBaseConnection
+const DataBaseConnection = async () => {
+  try {
+    //console.log("MONGO URI", process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log(`DataBase Connected`.bgBlue);
+  } catch (error) {
+    console.log(`Server Faild ${error}`.bgRed);
+  }
+};
+module.exports = DataBaseConnection;
